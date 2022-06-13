d86f97f679978aa67ef62ba39e70c6e0d145ffa0
LuisJS <wichin@outlook.com>
2022-06-13 13:17:32 -0500

    se agregaron nuevos archivos

--- Java/JavaBasicos/src/FlujoControl.java
+++ Java/JavaBasicos/src/FlujoControl.java
@@ -1,2 +1,3 @@
-package PACKAGE_NAME;public class FlujoControl {
+
+public class FlujoControl {
 }
--- Java/testGradle_2/lib/src/test/java/testGradle_2/CalculadoraTest.java
+++ Java/testGradle_2/lib/src/test/java/testGradle_2/CalculadoraTest.java
@@ -1,5 +1,6 @@
 package testGradle_2;
 
+import org.junit.Test;
 import org.junit.jupiter.api.*;
 
 public class CalculadoraTest {
